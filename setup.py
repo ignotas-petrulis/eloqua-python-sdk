c185fde223128df93ddd38e467f8fcb9658d6e80
Ignotas Petrulis <ignotas.petrulis@gmail.com>
2018-06-11 09:36:27 +0300

    version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ Eloqua Python SDK wrapping Eloqua Rest API and Eloqua BULK API.
 
 setup(
     name='eloqua-python-sdk',
-    version='0.1.4',
+    version='0.1.5',
     url='https://github.com/ignotas-petrulis/eloqua-python-sdk.git',
     author='Ignotas Petrulis',
     author_email='ignotas.petrulis@gmail.com',
